e9b8b6dbadfc060afa154e69663264e11c062333
DuaaH <dodohalaiqa.1@gmail.com>
2019-07-09 16:56:49 +0300

    data show correctly

--- src/handler.js
+++ src/handler.js
@@ -32,11 +32,11 @@ const publicHandler = (request, response, url) => {
     } else {
       response.writeHead(200, { "Content-Type": extensionType[extension] });
       // match fun(file)
-      response.end(result);
+      response.end(file);
     }
   });
 };
-const postHandler = (request, response) => {
+const postHandler = (request, response, url) => {
   const filePath = path.join(__dirname, "posts.json");
   fs.readFile(filePath, (error, file) => {
     if (error) {
@@ -45,12 +45,41 @@ const postHandler = (request, response) => {
       response.end("<h1>Sorry, we've had a problem on our end</h1>");
     } else {
       response.writeHead(200, { "Content-Type": "application/json" });
+      const arr = JSON.parse(file);
+      //const arr2 = JSON.stringify(file);
+
+      // findMatch(arr, findMatch(arr, url.split("/")[2]));
+      console.log(findMatch(arr, url.split("/")[2]));
       response.end(file);
-      console.log(file);
+      //  console.log(JSON.parse(file));
     }
   });
 };
 
+function findMatch(data, text) {
+  //console.log("array", data[0].title);
+
+  //  console.log("textt", text);
+  var newArr = [...data];
+  if (typeof text === "string") {
+    text = text.toUpperCase();
+  }
+  var arrayofresult = [];
+  var counter = 0;
+  for (var i = 0; i < newArr.length; i++) {
+    if (newArr[i].title.indexOf(text) === 0) {
+      arrayofresult.push(newArr[i].title);
+      counter++;
+      console.log(",,,,,");
+      if (counter == 5) {
+        break;
+      }
+    }
+  }
+  //console.log("textt", arrayofresult);
+
+  return arrayofresult;
+}
 module.exports = {
   homeHandler,
   publicHandler,
