6c9bae294151d0b3435efd3387e07f0c409c6f81
abcdefg <abc@def.ghi>
2019-07-09 14:48:30 +0300

    read from posts.json

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
-const { homeHandler, publicHandle } = require("./handler");
+const { homeHandler, publicHandler , postHandler} = require("./handler");
 const router = (request, response) => {
   const url = request.url;
   if (url === "/") {
     homeHandler(request, response);
   } else if (url.split(".")[1]) {
     console.log("hello");
-    publicHandle(request, response, url);
+    publicHandler(request, response, url);
   } else if (url.includes("/search")) {
+    postHandler(request, response, url);
+
     //  response.writeHead(200, { "Content-Type": "text/html" });
     console.log("url", url.split("/")[2]);
   } else {
