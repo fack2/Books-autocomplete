b4ad140627628152123afc493305efa20390240f
nadeen123 <nadeenshaheen.shahin@gmail.com>
2019-07-10 12:39:17 +0300

    script and style

--- public/script.js
+++ public/script.js
@@ -15,8 +15,29 @@ const textInput = document.getElementById("textInput");
 
 textInput.addEventListener("input", function() {
   console.log("dataa");
+
   const input = textInput.value;
-  request(`/search/${input}`, data => {
-    console.log("data", data);
-  });
+  if (input !== "") {
+    if (input[0] !== " " && event.keyCode !== 8) {
+      request(`/search/${input}`, data => {
+        // console.log("data", data);
+        var ul = document.createElement("ul");
+        ul.setAttribute("id", "ulid");
+        for (var i = 0; i < data.length; i++) {
+          var li = document.createElement("li");
+          li.setAttribute("class", "item");
+          var valueofarray = document.createTextNode(data[i]);
+          li.appendChild(valueofarray);
+          ul.appendChild(li);
+        }
+        var sec = document.getElementById("section-res");
+        sec.innerHTML = "";
+        sec.appendChild(ul);
+
+        if (text == "") {
+          ul.innerHTML = "";
+        }
+      });
+    }
+  }
 });
